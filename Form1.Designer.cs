e83385f35b7510ba50191e5c83e77cee16da1e10
Serhat Boyalı <serhat.boyali@outlook.com>
2024-08-27 15:03:03 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace OcaSıcaklığı
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.TextBox textBoxTemperature;
+        private System.Windows.Forms.Button buttonReadData;
+        private System.Windows.Forms.Label labelTemperature;
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.textBoxTemperature = new System.Windows.Forms.TextBox();
+            this.buttonReadData = new System.Windows.Forms.Button();
+            this.labelTemperature = new System.Windows.Forms.Label();
+            this.timerReadData = new System.Windows.Forms.Timer(this.components);
+            this.timerLogData = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelDurum = new System.Windows.Forms.Label();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(107, 12);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 33);
+            this.comboBoxPorts.TabIndex = 0;
+            this.comboBoxPorts.SelectedIndexChanged += new System.EventHandler(this.comboBoxPorts_SelectedIndexChanged);
+            // 
+            // textBoxTemperature
+            // 
+            this.textBoxTemperature.Location = new System.Drawing.Point(189, 210);
+            this.textBoxTemperature.Name = "textBoxTemperature";
+            this.textBoxTemperature.ReadOnly = true;
+            this.textBoxTemperature.Size = new System.Drawing.Size(176, 30);
+            this.textBoxTemperature.TabIndex = 2;
+            // 
+            // buttonReadData
+            // 
+            this.buttonReadData.Location = new System.Drawing.Point(12, 116);
+            this.buttonReadData.Name = "buttonReadData";
+            this.buttonReadData.Size = new System.Drawing.Size(216, 38);
+            this.buttonReadData.TabIndex = 3;
+            this.buttonReadData.Text = "Bağlan";
+            this.buttonReadData.UseVisualStyleBackColor = true;
+            this.buttonReadData.Click += new System.EventHandler(this.buttonReadData_Click);
+            // 
+            // labelTemperature
+            // 
+            this.labelTemperature.AutoSize = true;
+            this.labelTemperature.Location = new System.Drawing.Point(45, 213);
+            this.labelTemperature.Name = "labelTemperature";
+            this.labelTemperature.Size = new System.Drawing.Size(138, 25);
+            this.labelTemperature.TabIndex = 4;
+            this.labelTemperature.Text = "Oda Sıcaklığı :";
+            // 
+            // timerReadData
+            // 
+            this.timerReadData.Interval = 10000;
+            this.timerReadData.Tick += new System.EventHandler(this.timerReadData_Tick);
+            // 
+            // timerLogData
+            // 
+            this.timerLogData.Interval = 30000;
+            this.timerLogData.Tick += new System.EventHandler(this.timerLogData_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port Seç";
+            // 
+            // comboBoxBaudRate
+            // 
+            this.comboBoxBaudRate.FormattingEnabled = true;
+            this.comboBoxBaudRate.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaudRate.Location = new System.Drawing.Point(107, 60);
+            this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(121, 33);
+            this.comboBoxBaudRate.TabIndex = 1;
+            this.comboBoxBaudRate.SelectedIndexChanged += new System.EventHandler(this.comboBoxBaudRate_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Baut Hızı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(308, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "BAĞLANTI";
+            // 
+            // labelDurum
+            // 
+            this.labelDurum.AutoSize = true;
+            this.labelDurum.Location = new System.Drawing.Point(322, 55);
+            this.labelDurum.Name = "labelDurum";
+            this.labelDurum.Size = new System.Drawing.Size(83, 25);
+            this.labelDurum.TabIndex = 5;
+            this.labelDurum.Text = "KAPALI";
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(244, 116);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(216, 38);
+            this.buttonDisconnect.TabIndex = 3;
+            this.buttonDisconnect.Text = "Bağlantıyı Kes";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(472, 291);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelDurum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTemperature);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonReadData);
+            this.Controls.Add(this.textBoxTemperature);
+            this.Controls.Add(this.comboBoxBaudRate);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Oda Sıcaklığı";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Timer timerReadData;
+        private System.Windows.Forms.Timer timerLogData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelDurum;
+        private System.Windows.Forms.Button buttonDisconnect;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Modbus.Device;
+using System.IO.Ports;
+using System.IO;
+
+
+namespace OcaSıcaklığı
+{
+    public partial class Form1 : Form
+    {
+        private IModbusSerialMaster master;
+        private SerialPort port;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAvailablePorts();
+        }
+
+        private void LoadAvailablePorts()
+        {
+            comboBoxPorts.Items.Clear();
+            comboBoxPorts.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private void comboBoxPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedPort = comboBoxPorts.SelectedItem.ToString();
+            InitializeModbus(selectedPort, 9600);
+        }
+
+        private void comboBoxBaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedBaudRate = int.Parse(comboBoxBaudRate.SelectedItem.ToString());
+            string selectedPort = comboBoxPorts.SelectedItem.ToString();
+            InitializeModbus(selectedPort, selectedBaudRate);
+        }
+
+        private void buttonReadData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (port != null && port.IsOpen)
+                {
+                    labelDurum.Text = "AÇIK";
+                    // İlk veri okumasını başlat
+                    ReadTemperatureData();
+                    // Zamanlayıcıyı başlat
+                    timerReadData.Start();
+                    timerLogData.Start();
+                }
+                else
+                {
+                    labelDurum.Text = "KAPALI";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Seri port açılırken hata oluştu: {ex.Message}");
+            }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (port != null && port.IsOpen)
+                {
+                    labelDurum.Text = "KAPALI";
+                    timerReadData.Stop();
+                    timerLogData.Stop();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Seri port kapatılırken hata oluştu: {ex.Message}");
+            }
+
+        }
+
+        private void InitializeModbus(string portName, int baudRate)
+        {
+            try
+            {
+                if (port != null && port.IsOpen)
+                {
+                    port.Close();
+                }
+
+                port = new SerialPort(portName)
+                {
+                    BaudRate = baudRate,
+                    DataBits = 8,
+                    Parity = Parity.None,
+                    StopBits = StopBits.One
+                };
+
+                port.Open();
+                master = ModbusSerialMaster.CreateRtu(port);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Seri port açılırken hata oluştu: {ex.Message}");
+            }
+        }
+
+        private void ReadTemperatureData()
+        {
+            try
+            {
+                if (port != null && port.IsOpen)
+                {
+                    byte slaveId = 9;
+                    ushort startAddress = 4; // 5. register (0-indexed olarak 4)
+                    ushort numRegisters = 2; // 2 adet register okunacak
+
+                    // Modbus RTU üzerinden register okuma
+                    ushort[] registers = master.ReadHoldingRegisters(slaveId, startAddress, numRegisters);
+
+                    // Register'ları float değere dönüştür
+                    float temperature = ConvertRegistersToFloat(registers[1], registers[0]); // registers[1] yüksek byte, registers[0] düşük byte
+
+                    // Ekrana yazdır
+                    textBoxTemperature.Text = $"Oda Sıcaklığı: {temperature:F2} °C";
+                }
+                else
+                {
+                    MessageBox.Show("Seri port açık değil!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Veri okunurken hata oluştu: {ex.Message}");
+            }
+        }
+
+        private float ConvertRegistersToFloat(ushort highOrderByte, ushort lowOrderByte)
+        {
+            // İki adet 16-bit register'ı birleştir ve float'a dönüştür
+            ushort combinedValue = (ushort)((highOrderByte << 8) | lowOrderByte);
+
+            // Örnek dönüşüm: register değerini doğrudan float'a dönüştür
+            // Gereken dönüşüm işlemi yapılabilir
+            return combinedValue / 10.0f; // Örneğin, sıcaklık değeri 10'a bölünerek float değeri elde ediliyor
+        }
+
+        private void timerReadData_Tick(object sender, EventArgs e)
+        {
+            // Her zamanlayıcı tetiklenmesinde veri oku
+            ReadTemperatureData();
+        }
+        private void timerLogData_Tick(object sender, EventArgs e)
+        {
+            // Her saat başı tetiklendiğinde veri logla
+            LogTemperatureData();
+        }
+
+        private void LogTemperatureData()
+        {
+            try
+            {
+                string filePath = "temperature_log.txt";
+                string logEntry = $"{DateTime.Now}: {textBoxTemperature.Text}\n";
+
+                File.AppendAllText(filePath, logEntry);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Veri loglanırken hata oluştu: {ex.Message}");
+            }
+        }
+    }
+}
